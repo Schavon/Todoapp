7ab2e0ddb15b1f41586f5bdc5a5da87ac44c9669
Schavon <Alexandra.schavon@hotmail.com>
2019-02-27 21:01:08 +0100

    add items

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
 
-document.getElementById('add').onclick
 
-function buttonclick() {
-  console.log('Button clicked');
-
-}
+var removeIMG = '<img src="bin.jpg" alt=""></button>'
+var completeIMG = '<button class="complete"> <img src="com.jpg" alt=""></button>'
 
 document.getElementById('add').addEventListener('click', function(){
  var value = document.getElementById('item').value;
- if (value) {
-
- }
+ if (value) addItemTodo(value);
 });
+
+function addItemTodo(text) {
+  var list = document.getElementById('todo');
+  var item = document.createElement('li');
+  item.innerText = text;
+
+  var buttons = document.createElement('div');
+  buttons.classList.add('buttons');
+
+  var remove = document.createElement('button');
+  remove.classList.add('remove');
+  remove.innerHTML = removeIMG;
+
+  var complete = document.createElement('button');
+  complete.classList.add('complete');
+  complete.innerHTML = completeIMG;
+
+  buttons.appendChild(remove);
+  buttons.appendChild(complete);
+  item.appendChild(buttons);
+
+  list.appendChild(item);
+}
