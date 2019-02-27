016e92660fe9f4d96f2719721ea064ad1e93bb7f
Schavon <Alexandra.schavon@hotmail.com>
2019-02-27 21:53:24 +0100

    delete and marked buttons/items

--- main.js
+++ main.js
@@ -5,11 +5,34 @@ var completeIMG = '<button class="complete"> <img src="com.jpg" alt=""></button>
 
 document.getElementById('add').addEventListener('click', function(){
  var value = document.getElementById('item').value;
- if (value) addItemTodo(value);
+ if (value) {
+   addItemTodo(value);
+   document.getElementById('item').value = '';
+ }
 });
+
+function removeItem() {
+  var item = this.parentNode.parentNode;
+  var parent = item.parentNode;
+
+  parent.removeChild(item);
+}
+
+function completeItem() {
+  var item = this.parentNode.parentNode;
+  var parent = item.parentNode;
+  var id = parent.id;
+
+    var target = (id === 'todo') ? document.getElementById('completed'):document.getElementById('todo');
+
+     parent.removeChild(item);
+     target.insertBefore(item, target.childNodes[0]);
+}
+
 
 function addItemTodo(text) {
   var list = document.getElementById('todo');
+
   var item = document.createElement('li');
   item.innerText = text;
 
@@ -20,13 +43,19 @@ function addItemTodo(text) {
   remove.classList.add('remove');
   remove.innerHTML = removeIMG;
 
+  //click addEvent
+  remove.addEventListener('click', removeItem);
+
+
   var complete = document.createElement('button');
   complete.classList.add('complete');
   complete.innerHTML = completeIMG;
+
+  complete.addEventListener('click', completeItem);
 
   buttons.appendChild(remove);
   buttons.appendChild(complete);
   item.appendChild(buttons);
 
-  list.appendChild(item);
+  list.insertBefore(item, list.childNodes[0]);
 }
