22f46aac11d4273bd9fa390b99d640a22daa7467
Schavon <Alexandra.schavon@hotmail.com>
2019-02-27 20:34:19 +0100

    complete and remove btn

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('hej')
+
+document.getElementById('add').onclick
+
+function buttonclick() {
+  console.log('Button clicked');
+
+}
+
+document.getElementById('add').addEventListener('click', function(){
+ var value = document.getElementById('item').value;
+ if (value) {
+
+ }
+});
